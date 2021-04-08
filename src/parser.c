942b093af2c2c4d6463e078508182a9ae8f2ca82
tredfort <d.rinat703@gmail.com>
2021-04-09 00:49:40 +0300

    parser is ready

--- src/parser.c
+++ src/parser.c
@@ -6,80 +6,67 @@
 /*   By: tredfort <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 06:01:07 by tredfort          #+#    #+#             */
-/*   Updated: 2021/04/08 06:01:08 by tredfort         ###   ########.fr       */
+/*   Updated: 2021/04/09 00:45:16 by tredfort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #define SPLITTER (-1)
 
-void	print(t_list *lst)
+static void	add_instruction(char *line, int start, t_list **lst)
 {
-	t_list	*tmp;
-	t_list	*head;
-	t_sh	*cmd;
+	char	*new;
 	int		i;
 
-	head = lst;
-	while (lst)
-	{
-		cmd = lst->content;
-		printf("cmd: %s\n", cmd->argv[0]);
-		i = 1;
-		while (cmd->argv[i])
-		{
-			printf("argv%d: %s\n", i, cmd->argv[i]);
-			i++;
-		}
-		if (cmd->pipe)
-			printf("pipe: yes\n");
-		else
-			printf("pipe: no\n");
-		tmp = cmd->redir;
-		while (tmp)
-		{
-			printf("%s\n", (char *)tmp->content);
-			free(tmp->content);
-			tmp = tmp->next;
-		}
-		free(cmd);
-		lst = lst->next;
-	}
-	ft_lstclear(&head, free);
+	i = start + 2;
+	while (line[i] == ' ')
+		i++;
+	while (line[i] && !ft_strchr(" <>", line[i]))
+		i++;
+	new = ft_substr(line, start, i - start);
+	ft_memset(line + start, ' ', i - start);
+	ft_lstadd_back(lst, ft_lstnew(new));
 }
 
-void	search_redirects(char *line, t_list **lst)
+static void	search_redirects(char *line, t_list **lst)
 {
 	int		i;
-	int		start;
-	char	*new;
 
 	i = 0;
 	while (line[i])
 	{
 		while (line[i] == ' ')
-			line[i++] = SPLITTER;
+			i++;
 		if (line[i] == '"' || line[i] == '\'')
 			skip_quotes(line, &i);
-		if (!ft_strncmp(line + i, ">>", 2))
-		{
-			start = i;
-			while (line[i] == ' ')
-				i++;
-			while (line[i] && !ft_strchr(" <>", line[i]))
-				i++;
-			new = ft_substr(line, start, i - start);
-			ft_memset(line + start, SPLITTER, i - start);
-			ft_lstadd_back(lst, ft_lstnew(new));
-		}
-		if (line[i] == '\\' && ft_strchr("'\"|\\<>$; ", line[i + 1]))
+		if (line[i] && ft_strchr("<>", line[i]))
+			add_instruction(line, i, lst);
+		else if (line[i] == '\\' && ft_strchr("'\"|\\<>$; ", line[i + 1]))
 			i += 2;
 		else if (line[i] && !ft_strchr("'\"", line[i]))
 			i++;
 	}
 }
 
-t_sh	*add_new_command(char *line, int len, int pipe)
+static void	add_splitter(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] == ' ')
+			str[i++] = SPLITTER;
+		if (str[i] == '"' || str[i] == '\'')
+			skip_quotes(str, &i);
+		if (str[i] == '\\' && ft_strchr("'\"|\\<>$; ", str[i + 1]))
+			i += 2;
+		else if (str[i] && !ft_strchr("'\"", str[i]))
+			i++;
+	}
+}
+
+static t_sh	*add_new_command(char *line, int len, int pipe)
 {
 	t_sh	*cmd;
 	char	*tmp;
@@ -93,12 +80,13 @@ t_sh	*add_new_command(char *line, int len, int pipe)
 	cmd->pipe = pipe;
 	tmp = ft_substr(line, 0, len);
 	search_redirects(tmp, &cmd->redir);
+	add_splitter(tmp);
 	cmd->argv = ft_split(tmp, SPLITTER);
 	free(tmp);
 	return (cmd);
 }
 
-void	parser(char *line, t_list *lst)
+void	parser(char *line, t_list **lst)
 {
 	t_sh	*cmd;
 	int		i;
@@ -106,6 +94,7 @@ void	parser(char *line, t_list *lst)
 
 	i = 0;
 	start = 0;
+	*lst = NULL;
 	while (line[i])
 	{
 		if (line[i] == '"' || line[i] == '\'')
@@ -116,7 +105,7 @@ void	parser(char *line, t_list *lst)
 				cmd = add_new_command(line + start, i - start, 0);
 			else if (line[i] == '|')
 				cmd = add_new_command(line + start, i - start, 1);
-			ft_lstadd_back(&lst, ft_lstnew(cmd));
+			ft_lstadd_back(lst, ft_lstnew(cmd));
 			start = i + 1;
 		}
 		if (line[i] == '\\' && ft_strchr("'\"|\\<>$; ", line[i + 1]))
@@ -124,5 +113,4 @@ void	parser(char *line, t_list *lst)
 		else if (line[i] && !ft_strchr("'\"", line[i]))
 			i++;
 	}
-	free(line);
 }
